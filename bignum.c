56b5d4866c7751211188f7a939c422cfbf4bbb92
DW0RKiN <ogion@centrum.cz>
2017-07-25 18:52:44 +0200

    Update bignum.c

--- bignum.c
+++ bignum.c
@@ -3,32 +3,9 @@
 #include "bignum.h"
 
 
-
-// ( num == 0 )
-int test_zero (my_number * num)
-{
-	int i;
-
-	for (i = 0; i < SUM_FULL; i++)
-		if (num->full[i])
-			return 0;
-	return 1;
-}
-
-// a >  b return  1
-// a == b return  0
-// a <  b return -1
-int test_compare (my_number * a, my_number * b)
-{
-	int i;
-
-	for (i = SUM_FULL-1; i >= 0; i--)
-		if (a->full[i] > b->full[i])
-			return 1;
-		else if (a->full[i] < b->full[i]) 
-			return -1;
-	return 0;
-}
+/***********************
+ *  Nastaveni hodnoty  *
+ ***********************/
 
 
 // res = 0
@@ -38,16 +15,6 @@ void set_zero (my_number * res)
 
 	for (i = 0; i < SUM_FULL; i++)
 		res->full[i] = 0;
-}
-
-
-// res = num
-void copy (my_number * res, my_number * num)
-{
-	int i;
-
-	for (i = 0; i < SUM_FULL; i++)
-		res->full[i] = num->full[i];
 }
 
 
@@ -79,330 +46,19 @@ void set_from_string (my_number * res, char * text)
 }
 
 
-// res <<= 1
-int shift_left_1 ( my_number * res, int old_carry )
-{
-	int i;
-	my_full carry;
-
-	for (i = 0; i < SUM_FULL; i++) {
-		carry = old_carry; 
-		old_carry = res->full[i] >> (8 * sizeof (my_full) - 1) ;
-		res->full[i] <<= 1;
-		res->full[i] |= carry;
-	}
-	return old_carry;
-}
-
-
-// res >>= 1
-int  shift_right_1 ( my_number * res, int old_carry )
-{
-	int i;
-	my_full carry;
-
-	for (i = SUM_FULL - 1; i >= 0; i--) {
-		carry = ( old_carry ? (my_full) 1 << (8 * sizeof (my_full) - 1) : 0);
-		old_carry = res->full[i] & 1;
-		res->full[i] >>= 1;
-		res->full[i] |= carry;
-	}
-	return old_carry;
-}
-
-
-// res <<= kolik
-// return carry (.... ..11)
-// 0 <= kolik < 8*sizeof(my_full)
-my_full left (my_number * res, int kolik, my_full carry)
-{
-	int i;
-	my_full x;
-
-	for (i = 0; i < SUM_FULL; i++) {
-		x = res->full[i];
-		res->full[i] <<= kolik;
-		res->full[i] |= carry;
-		carry = x >> (8 * sizeof(my_full) - kolik); 
-	}
-
-	return carry;
-}
-
-
-// res >>= kolik
-// return carry (11.. ....)
-// 0 <= kolik < 8*sizeof(my_full)
-my_full right (my_number * res, int kolik, my_full carry)
-{
-	int i;
-	my_full x;
-
-	for (i = SUM_FULL - 1; i >= 0; i--) {
-		x = res->full[i];
-		res->full[i] >>= kolik;
-		res->full[i] |= carry;
-		carry = x << (8 * sizeof (my_full) - kolik);
-	}
-
-	return carry;
-}
-
-
-// res--
-// return carry
-my_half dec (my_number * res)
+// res = num
+void copy (my_number * res, my_number * num)
 {
 	int i;
 
-	for (i = 0; i < SUM_FULL; i++) {
-		if (res->full[i]--)
-			return 0;
-	}
-	return 1;
+	for (i = 0; i < SUM_FULL; i++)
+		res->full[i] = num->full[i];
 }
 
 
-// res++
-// return carry
-my_half inc (my_number * res)
-{
-	int i;
-
-	for (i = 0; i < SUM_FULL; i++) {
-		if (++res->full[i])
-			return 0;
-	}
-	return 1;
-}
-
-
-// res = a - b
-// return ( a < b )
-// &res == &a == &b je povoleno
-my_half sub (my_number * res, my_number * a, my_number * b)
-{
-	int i;
-	my_full x = 0;
-	my_half carry = 0;
-
-	for (i = 0; i < SUM_FULL; i++) {
-		x = a->full[i];
-		res->full[i] = a->full[i] - b->full[i] - carry;
-		carry = 0;
-		if ( x > res->full[i] ) continue;
-		if ( res->full[i] == x && b->full[i] == 0 ) continue;
-		carry = 1;
-	}
-	return carry;
-}
-
-
-// res = res / half
-// return res % half
-my_half div_half (my_number * res, my_half half)
-{
-	int i;
-	my_full x = 0;
-	my_half *px = (my_half *) & x;
-
-	for (i = SUM_HALF - 1; i >= 0; i--) {
-		px[1] = px[0];
-		px[0] = res->half[i];
-		res->half[i] = x / half;
-		x = x % half;
-	}
-
-	return px[0];
-}
-
-
-// res <<= x
-// return x
-// hi_byte (res) != 0
-int left_to_nonzero (my_number *res) 
-{
-	int ret,j,k;
-	char *str = (char *) res;
-	
-	// Finding the first non-zero element
-	ret = 0;
-	j = sizeof (my_number) -1;
-	do {
-		if ( str[j] ) break;
-		ret += 8;
-	} while (j--);
-	
-	if ( ret == 0 || j < 0 ) return ret;
-
-	k = sizeof (my_number);
-	do str[--k] = str[j]; while ( j-- );
-	while ( k--) str[k] = 0;
-
-	return ret;
-}
-
-
-// res = res / num
-// return ( (res % num) == 0 )
-// &res == &num NENI povoleno
-int div (my_number * res, my_number * num)
-{
-	int i;
-	my_number m, n;
-	my_number *pm = &m, *pn = &n;
-	my_full carry = 0;
-
-	set_zero (pm);
-
-//	m << res << !(m - num)
-
-	i = left_to_nonzero( res );
-	for (; i < 8 * sizeof (my_number); i++) {	// rychlostni optimalizace
-		carry = shift_left_1 (res, carry);
-		if ( carry ) break;
-	}
-	
-	for (; i < 8 * sizeof (my_number); i++) {
-		shift_left_1 (pm, carry);
-		carry = 1 - sub (pn, pm, num);
-		if (carry) {
-			my_number *temp;
-			temp = pm;
-			pm = pn;
-			pn = temp;
-		}
-		carry = shift_left_1 (res, carry);
-	}
-
-	return test_zero (pm);		// pozor! diky prohazovani to muze ukazovat na n
-}
-
-
-// res = res + ( num << (i+1)*8*sizeof(my_half))
-// Pozor! i bude hned zvyseno
-void add_half (my_number * res, my_half num, int i)
-{
-	my_full m;
-	my_half *pm = (my_half *) & m;
-	pm[1] = num;
-
-	while (pm[1]) {
-		i++;
-		if (i >= SUM_HALF) {
-			fprintf (stderr, "Preteceni! Zvyste SUM_FULL.\n");
-			break;
-		}
-		pm[0] = pm[1];
-		pm[1] = 0;
-
-		m += res->half[i];
-		res->half[i] = pm[0];
-	}
-}
-
-
-// res = a + b
-// return carry
-// &res == &a == &b je povoleno
-my_half add (my_number * res, my_number * a, my_number * b)
-{
-	int i;
-	my_full x;
-	my_half carry = 0;
-
-	for (i = 0; i < SUM_FULL; i++) {
-		x = a->full[i];
-		res->full[i] = a->full[i] + b->full[i] + carry;
-		carry = 0;
-		if ( res->full[i] > x ) continue;
-		if ( res->full[i] == x && b->full[i] == 0 ) continue;
-		carry = 1;
-	}
-
-	return carry;
-}
-
-
-// res = res*half
-void mul_half (my_number * res, my_half half)
-{
-	int i;
-	my_full m;
-	my_half *pm = (my_half *) & m;
-
-	for (i = SUM_HALF - 1; i >= 0; i--) {
-		m = half;
-		m *= res->half[i];
-		res->half[i] = pm[0];
-		add_half (res, pm[1], i);
-	}
-}
-
-
-// res = a*b
-// &res == &const_a == &const_b je povoleno
-void mul (my_number * res, my_number * const_a, my_number * const_b)
-{
-	my_number a,b;
-	if ( test_compare( const_a, const_b ) > 0 ) {
-		copy( &a, const_b );
-		copy( &b, const_a );
-	} else {
-		copy( &a, const_a );
-		copy( &b, const_b );
-	}
-	set_zero (res);					// musi byt posledni kvuli &res == &const_a == &const_b
-	
-	while ( ! test_zero( &a) ) {
-		if ( shift_right_1 ( &a, 0 )) add(res,res,&b);
-		if ( shift_left_1 ( &b, 0 )) fprintf(stderr, "Preteceni pri nasobeni!\n");
-	}
-}
-
-
-// res = res^pow
-void power (my_number * res, unsigned int pow)
-{
-	my_number temp;
-	
-	copy(&temp, res);
-	set_zero(res);
-	res->half[0]++;		// res = 1
-	
-	while ( pow ) {
-		if ( pow & 1 ) mul (res, res, &temp);
-		mul (&temp, &temp, &temp);
-		pow >>= 1;
-	}
-	return;
-}
-
-
-// res = sqrt(num)
-// &res == &num je povoleno
-void sqr (my_number * res, my_number * num)
-{
-	my_number bit, temp, n;
-	copy(&n, num);
-	set_zero (res);
-	set_zero (&bit);
-	bit.half[SUM_HALF - 1] = (my_half) 1 << (8 * sizeof (my_half) - 2);
-	
-	while (test_compare (&bit, &n) == 1 )	// rychlostni optimalizace
-		right (&bit, 2, 0);
-
-	while (!test_zero (&bit)) {
-		add (&temp, res, &bit);
-		shift_right_1 (res, 0);
-
-		if (!sub (&temp, &n, &temp)) {
-			copy (&n, &temp);
-			add (res, res, &bit);
-		}
-		right (&bit, 2, 0);
-	}
-}
+/*******************
+ *  Vypis hodnoty  *
+ *******************/
 
 
 // res = 0
@@ -448,3 +104,347 @@ void print_hex_number (FILE *stream, char * before, my_number * num, char * afte
 }
 
 
+/*******************
+ *  Testy hodnoty  *
+ *******************/
+
+
+// ( num == 0 )
+int test_zero (my_number * num)
+{
+	int i;
+
+	for (i = 0; i < SUM_FULL; i++)
+		if (num->full[i])
+			return 0;
+	return 1;
+}
+
+// a >  b return  1
+// a == b return  0
+// a <  b return -1
+int test_compare (my_number * a, my_number * b)
+{
+	int i;
+
+	for (i = SUM_FULL-1; i >= 0; i--)
+		if (a->full[i] > b->full[i])
+			return 1;
+		else if (a->full[i] < b->full[i]) 
+			return -1;
+	return 0;
+}
+
+
+/************************
+ *  Scitani a odcitani  *
+ ************************/
+
+
+// res--
+// return carry
+my_half dec (my_number * res)
+{
+	int i;
+
+	for (i = 0; i < SUM_FULL; i++) {
+		if (res->full[i]--)
+			return 0;
+	}
+	return 1;
+}
+
+
+// res++
+// return carry
+my_half inc (my_number * res)
+{
+	int i;
+
+	for (i = 0; i < SUM_FULL; i++) {
+		if (++res->full[i])
+			return 0;
+	}
+	return 1;
+}
+
+
+// res = res + ( num << (i+1)*8*sizeof(my_half))
+// Pozor! i bude hned zvyseno
+void add_half (my_number * res, my_half num, int i)
+{
+	my_full m;
+	my_half *pm = (my_half *) & m;
+	pm[1] = num;
+
+	while (pm[1]) {
+		i++;
+		if (i >= SUM_HALF) {
+			fprintf (stderr, "Preteceni! Zvyste SUM_FULL.\n");
+			break;
+		}
+		pm[0] = pm[1];
+		pm[1] = 0;
+
+		m += res->half[i];
+		res->half[i] = pm[0];
+	}
+}
+
+
+// res = a + b
+// return carry
+// &res == &a == &b je povoleno
+my_half add (my_number * res, my_number * a, my_number * b)
+{
+	int i;
+	my_full x;
+	my_half carry = 0;
+
+#if 0
+	x = 0;
+	
+	for (i = 0; i < SUM_FULL; i++) {
+
+		carry  = __builtin_add_overflow (a->full[i], b->full[i], &res->full[i]);
+		carry += __builtin_add_overflow (x, res->full[i], &res->full[i]);
+		x = carry;
+#else
+	for (i = 0; i < SUM_FULL; i++) {
+
+		x = a->full[i];
+		res->full[i] = a->full[i] + b->full[i] + carry;
+		carry = 0;
+		if ( res->full[i] > x ) continue;
+		if ( res->full[i] == x && b->full[i] == 0 ) continue;
+		carry = 1;
+#endif
+
+	}
+
+	return carry;
+}
+
+
+// res = a - b
+// return ( a < b )
+// &res == &a == &b je povoleno
+my_half sub (my_number * res, my_number * a, my_number * b)
+{
+	int i;
+	my_full x = 0;
+	my_half carry = 0;
+
+	for (i = 0; i < SUM_FULL; i++) {
+		x = a->full[i];
+		res->full[i] = a->full[i] - b->full[i] - carry;
+		carry = 0;
+		if ( x > res->full[i] ) continue;
+		if ( res->full[i] == x && b->full[i] == 0 ) continue;
+		carry = 1;
+	}
+	return carry;
+}
+
+
+/************
+ *  Posuny  *
+ ************/
+
+
+// res <<= kolik
+// return carry (.... ..11)
+// 0 <= kolik < 8*sizeof(my_half)
+my_half shift_left ( my_number * res, int kolik, my_half carry )
+{
+	my_mix n, *p = (my_mix *) res;
+	my_half new_carry;
+	int i = SUM_FULL;
+
+	while (i--) {
+		n = *p;
+		new_carry = n.h[1] >> (8*sizeof(my_half) - kolik);
+		n.f <<= kolik;
+		n.h[0] |= carry;
+		*p++ = n;
+		carry = new_carry;
+	}
+	
+	return carry;
+}
+
+
+// res >>= kolik
+// return carry (11.. ....)
+// 0 <= kolik < 8*sizeof(my_half)
+my_half shift_right ( my_number * res, int kolik, my_half carry )
+{
+	my_mix n, *p = (my_mix *) res + SUM_FULL;
+	my_half new_carry;
+	int i = SUM_FULL;
+// 	kolik &= 8*sizeof(my_half) - 1;
+
+	while (i--) {
+		n = *--p;
+		new_carry = n.h[0] << (8*sizeof(my_half) - kolik);
+		n.f >>= kolik;
+		n.h[1] |= carry;
+		*p = n;
+		carry = new_carry;
+	}
+	return carry;
+}
+
+
+/***********************
+ *  Nasobeni a deleni  *
+ ***********************/
+
+
+// res = res*half
+void mul_half (my_number * res, my_half half)
+{
+	int i;
+	my_full m;
+	my_half *pm = (my_half *) & m;
+
+	for (i = SUM_HALF - 1; i >= 0; i--) {
+		m = half;
+		m *= res->half[i];
+		res->half[i] = pm[0];
+		add_half (res, pm[1], i);
+	}
+}
+
+
+// res = res / half
+// return res % half
+my_half div_half (my_number * res, my_half half)
+{
+	int i;
+	my_full x = 0;
+	my_half *px = (my_half *) & x;
+
+	for (i = SUM_HALF - 1; i >= 0; i--) {
+		px[1] = px[0];
+		px[0] = res->half[i];
+		res->half[i] = x / half;
+		x = x % half;
+	}
+
+	return px[0];
+}
+
+
+// res = a*b
+// &res == &const_a == &const_b je povoleno
+void mul (my_number * res, my_number * const_a, my_number * const_b)
+{
+	my_number a,b;
+	if ( test_compare( const_a, const_b ) > 0 ) {
+		copy( &a, const_b );
+		copy( &b, const_a );
+	} else {
+		copy( &a, const_a );
+		copy( &b, const_b );
+	}
+	set_zero (res);					// musi byt posledni kvuli &res == &const_a == &const_b
+	
+	while ( ! test_zero( &a) ) {
+		if ( shift_right ( &a, 1, 0 )) add(res,res,&b);
+		if ( shift_left ( &b, 1, 0 )) fprintf(stderr, "Preteceni pri nasobeni!\n");
+	}
+}
+
+
+// res = res / num
+// return ( (res % num) == 0 )
+// &res == &num NENI povoleno
+int div (my_number * res, my_number * num)
+{
+	int i;
+	my_number m, n;
+	my_number *pm = &m, *pn = &n;
+	my_full carry = 0;
+
+	set_zero (pm);
+
+//	m << res << !(m - num)
+
+	i = left_to_nonzero( res );
+	for (; i < 8 * sizeof (my_number); i++) {	// rychlostni optimalizace
+		carry = shift_left (res, 1, carry);
+		if ( carry ) break;
+	}
+	
+	for (; i < 8 * sizeof (my_number); i++) {
+		shift_left (pm, 1, carry);
+		carry = 1 - sub (pn, pm, num);
+		if (carry) {
+			my_number *temp;
+			temp = pm;
+			pm = pn;
+			pn = temp;
+		}
+		carry = shift_left (res, 1, carry);
+	}
+
+	return test_zero (pm);		// pozor! diky prohazovani to muze ukazovat na n
+}
+
+
+/***************
+ *  Specialni  *
+ ***************/
+
+
+// res = sqrt(num)
+// &res == &num je povoleno
+void sqr (my_number * res, my_number * num)
+{
+	my_number bit, temp, n;
+	copy(&n, num);
+	set_zero (res);
+	set_zero (&bit);
+	bit.half[SUM_HALF - 1] = (my_half) 1 << (8 * sizeof (my_half) - 2);
+	
+	while (test_compare (&bit, &n) == 1 )	// rychlostni optimalizace
+		shift_right (&bit, 2, 0);
+
+	while (!test_zero (&bit)) {
+		add (&temp, res, &bit);
+		shift_right (res, 1, 0);
+
+		if (!sub (&temp, &n, &temp)) {
+			copy (&n, &temp);
+			add (res, res, &bit);
+		}
+		shift_right (&bit, 2, 0);
+	}
+}
+
+
+// res <<= x
+// return x
+// hi_byte (res) != 0
+int left_to_nonzero (my_number *res) 
+{
+	int ret,j,k;
+	char *str = (char *) res;
+	
+	// Finding the first non-zero element
+	ret = 0;
+	j = sizeof (my_number) -1;
+	do {
+		if ( str[j] ) break;
+		ret += 8;
+	} while (j--);
+	
+	if ( ret == 0 || j < 0 ) return ret;
+
+	k = sizeof (my_number);
+	do str[--k] = str[j]; while ( j-- );
+	while ( k--) str[k] = 0;
+
+	return ret;
+}
